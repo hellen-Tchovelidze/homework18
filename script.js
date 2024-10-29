60ac04b81223cbaaf0ec5e3038836d654a49c201
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-10-29 21:31:44 +0400

    js

--- script.js
+++ script.js
@@ -4,17 +4,35 @@ let time = prompt("enter time of day(matinee or evening)");
 let btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
   switch (true) {
+    case age <= 10 && type === "3D" && time === "evening":
+      alert("ბილეთის ფასი იქნება 15 ლარი");
+      break;
+    case age <= 10 && type === "3D":
+      alert("ბილეთის ფასი იქნება 10 ლარი");
+      break;
     case age <= 10:
-      alert = "ბილეთის ფასი 5 ლარი";
+      alert("ბილეთის ფასი 5 ლარი");
+      break;
+    case age > 10 && age <= 65 && type === "3D" && time === "evening":
+      alert("ბილეთის ფასი 25 ლარი");
+      break;
+    case age > 10 && age <= 65 && type === "3D":
+      alert("ბილეთის ფასი 20 ლარი");
       break;
     case age > 10 && age <= 65:
-      alert = "biletis fasi 15";
+      alert("ბილეთის ფასი 15 ლარი");
+      break;
+    case age > 65 && type === "3D" && time === "evening":
+      alert("ბილეთის ფასი 20 ლარი");
+      break;
+    case age > 65 && type === "3D":
+      alert("ბილეთის ფასი 15 ლარი");
       break;
     case age > 65:
-      alert = "bileti 10";
+      alert("ბილეთის ფასი 10 ლარი");
       break;
+
     default:
       break;
   }
-  
 });
